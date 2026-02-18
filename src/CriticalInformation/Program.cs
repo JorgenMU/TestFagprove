40e601890c5160e2cd83e976f29b13293ef2d2f3
jorgulv <jorgen.moseid.ulven@nhn.no>
2026-02-18 16:01:34 +0100

    simple patient endpoint

--- src/CriticalInformation/Program.cs
+++ src/CriticalInformation/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using CriticalInformation.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("/patient/{id:int}", (int id) =>
+{
+    var patient = new Patient
+    {
+        Id = id,
+        Name = "Ola Normann",
+        DateOfBirth = DateTimeOffset.Now,
+        CriticalInformation = []
+    };
+
+    return patient;
+});
+
+app.Run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
